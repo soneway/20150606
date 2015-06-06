7841f1df0d8d35f3c022cb0768ce70cc45084606
soneway <250084439@qq.com>
2015-06-06 15:56:38 +0800

    生成页面

---
+++ gulpfile.js
@@ -0,0 +1,65 @@
+//npm install gulp gulp-jshint gulp-sass gulp-concat gulp-uglify gulp-rename --save-dev
+
+var debug = 1;
+
+//引入gulp
+var gulp = require('gulp');
+
+
+////检查脚本
+//var jshint = require('gulp-jshint');
+//gulp.task('lint', function () {
+//    gulp.src('./js/!*.js')
+//        .pipe(jshint())
+//        .pipe(jshint.reporter('default'));
+//});
+
+
+//编译sass,压缩css
+var sass = require('gulp-sass');
+var minifyCss = require('gulp-minify-css');
+gulp.task('css', function () {
+    var task = gulp.src('css/index.scss')
+        //编译
+        .pipe(sass())
+        .pipe(gulp.dest('out'));
+
+    //压缩
+    if (!debug) {
+        task.pipe(minifyCss())
+            .pipe(gulp.dest('out'));
+    }
+});
+
+
+//合并,压缩文件js
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+//var rename = require('gulp-rename');
+gulp.task('js', function () {
+    //合并
+    var task = gulp.src(['js/lib/soneway.js', 'js/lib/ui.js', 'js/index.js'])
+        .pipe(concat('index.js'))
+        .pipe(gulp.dest('out'));
+
+    //压缩
+    if (!debug) {
+        task.pipe(uglify())
+            .pipe(gulp.dest('out'));
+    }
+});
+
+
+//默认任务
+gulp.task('default', function () {
+    //监听文件变化
+    gulp.watch(['css/**'], function () {
+        gulp.run('css');
+    });
+    gulp.watch(['js/**'], function () {
+        gulp.run('js');
+    });
+});
+
+//初始化运行
+gulp.run(['css', 'js']);
