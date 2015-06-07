b42ab5303f33759fc6edefa5ceecbebc1bcac79c
soneway <250084439@qq.com>
2015-06-07 12:03:08 +0800

    生成页面

--- out/index.js
+++ out/index.js
@@ -1325,9 +1325,35 @@
     });
 
 })(this, $);
+(function (window, $) {
 
-(function (window) {
+    //文档jq对象
+    var $doc = $(window.document),
+        load = {}, unload = {};
 
 
+    //文档加载完成
+    $(function () {
+        $.toggleSidebox(true);
+    });
 
-})(this);
+
+    //面板显示回调函数
+    $.panelLoaded = function ($this, isInit) {
+        var loadfn = load[$this.attr('id')];
+        $.isFunction(loadfn) && loadfn($this, isInit);
+    };
+
+    //面板隐藏回调函数
+    $.panelUnloaded = function ($this) {
+        var unloadfn = unload[$this.attr('id')];
+        $.isFunction(unloadfn) && unloadfn($this);
+    };
+
+
+    //客服中心
+    load.sidebox = (function () {
+
+    })();
+
+})(this, $);
